0a00ded131272c957575b5c5464620f8d81076fe
Thyago Erick <thyago123gois@gmail.com>
2022-11-12 16:16:40 -0300

    Primeiro teste concluído com sucesso

--- Empregado.java
+++ Empregado.java
@@ -42,4 +42,29 @@ public class Empregado{
 	public int tipo (){
 		return this.tipo;
 	}
+
+	public void setSalario (double salario){
+		this.salario = salario;
+	}
+
+	public double getSalario (){
+		return this.salario;
+	}
+
+	public void setComissao (double comissao){
+		this.comissao = comissao;
+	}
+
+	public double getComissao (){
+		return this.comissao;
+	}
+
+	public void setBonus (double bonus){
+		this.bonus = bonus;
+	}
+
+	public double getBonus (){
+		return this.bonus;
+	}
+
 }
